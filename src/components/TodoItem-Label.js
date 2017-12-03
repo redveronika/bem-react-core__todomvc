93574cf766f4e88b9605e1c74e8a406765509999
Veronika Voronova <voronovaveronika@gmail.com>
2017-12-04 00:13:28 +0300

    Элемент Label

---
+++ src/components/TodoItem-Label.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { decl } from 'bem-react-core';
+
+export default decl({
+    block: 'TodoItem',
+    elem: 'Label',
+
+    tag: 'label',
+
+    attrs({ onClick }) {
+        return {
+            onClick
+        }
+    }
+}, {
+    propTypes: {
+        onClick: PropTypes.func,
+    },
+});
