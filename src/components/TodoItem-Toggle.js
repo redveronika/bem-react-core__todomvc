284b704b9bb87de89a07fec261727019562919d4
Veronika Voronova <voronovaveronika@gmail.com>
2017-12-04 00:04:26 +0300

    Сделала элемент Toggle для блока TodoItem

--- src/components/TodoItem-Toggle.js
+++ src/components/TodoItem-Toggle.js
@@ -1,3 +1,23 @@
-/**
- * Created by VVoronova on 12/3/2017.
- */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { decl } from 'bem-react-core';
+
+export default decl({
+    block: 'TodoItem',
+    elem: 'Toggle',
+
+    tag: 'input',
+
+    attrs({ checked, onChange }) {
+        return {
+            type: 'checkbox',
+            checked,
+            onChange
+        }
+    }
+}, {
+    propTypes: {
+        checked: PropTypes.bool,
+        onChange: PropTypes.func,
+    },
+});
